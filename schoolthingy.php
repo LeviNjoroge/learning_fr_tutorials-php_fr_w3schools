5ae3a8015387d8680c3fa331710739a5d7137670
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-06 21:19:14 +0300

    still learning php

--- schoolthingy.php
+++ schoolthingy.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>School</title>
 </head>
 <body>
-    
+    <form action="schoolthingy.php" method="POST">
+        <input type="text" name="regno" id="regno" placeholder="Registration number:"> <br>
+        <input type="text" name="name" id="name" placeholder="Name: "> <br>
+        <input type="submit" value="Submit"> <br>
+    </form>
 </body>
 </html>
+
+<?php 
+    $reg
+?>
